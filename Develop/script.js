6231a95e18d215ee8175b7620590eba3fcb6c777
Paige A <pvaltemare@gmail.com>
2021-09-02 13:48:42 -0700

    simplify character types and selection

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,3 @@
-//Character Arrays
-var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-var specialCharacterArray = ['!', '@', '#', '$', '%', '&', '*', '+', '_'];
-
 //Prompt for the length of the password, at least 8 characters and no more than 128 characters
 var getPasswordLength = function() {
   var passwordLength = 0;
@@ -14,36 +8,41 @@ var getPasswordLength = function() {
   return passwordLength;
 };
 
+//Character Types
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "1234567890";
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+//Source: https://owasp.org/www-community/password-special-characters
+
 //Confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 var getValidCharacters = function() {
-  var validCharacters = [];
-  var validCharacterSelection = false;
-  while (!validCharacterSelection) {
-    characterTypeResponse = window.prompt("Password Criteria: Please choose which character types to include in the password: LOWERCASE, UPPERCASE, NUMERIC, and/or SPECIAL CHARACTERS");
-    characterTypeResponse = characterTypeResponse.toLowerCase();
-    if (characterTypeResponse.includes('lowercase') || characterTypeResponse.includes('uppercase') || characterTypeResponse.includes('numeric') || characterTypeResponse.includes('special characters')) {
-      validCharacterSelection = true;
-    }
-  }
+  var validCharacters = "";
+  characterTypeResponse = window.prompt("Password Criteria: Please choose which character types to include in the password: LOWERCASE, UPPERCASE, NUMERIC, and/or SPECIAL CHARACTERS");
+
   if (characterTypeResponse.includes('lowercase')) {
-    validCharacters = validCharacters.concat(lowerCaseArray);
+    validCharacters = validCharacters + lowerCase;
   }
   if (characterTypeResponse.includes('uppercase')) {
-    validCharacters = validCharacters.concat(upperCaseArray);
+    validCharacters = validCharacters + upperCase;
   }
   if (characterTypeResponse.includes('numeric')) {
-    validCharacters = validCharacters.concat(numericArray);
+    validCharacters = validCharacters + numeric;
   }
   if (characterTypeResponse.includes('special characters')) {
-    validCharacters = validCharacters.concat(specialCharacterArray);
+    validCharacters = validCharacters + specialCharacters;
   }
+  if (!validCharacters) {
+    return getValidCharacters();
+  }
+
   return validCharacters;
 };
 
 var generatePassword = function() {
   //Default Criteria
   var passwordLength = 8;
-  var validCharacters = lowerCaseArray;
+  var validCharacters = lowerCase;
 
   //Prompt for Password Criteria
   var passwordCriteria = window.prompt("Which criteria would you like to include in the generated password? Please enter one 1 for PASSWORD LENGTH, 2 for CHARACTER TYPE, or 3 for BOTH.");  
