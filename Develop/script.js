f9866d749c7c713a6eb703e40498bf52f4fa26a1
Paige A <pvaltemare@gmail.com>
2021-09-02 00:12:31 -0700

    clean up code

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,10 @@
-// Assignment code here
-//Constants
+//Character Arrays
 var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numericArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var specialCharacterArray = ['!', '@', '#', '$', '%', '&', '*', '+', '_'];
 
-
+//Prompt for the length of the password, at least 8 characters and no more than 128 characters
 var getPasswordLength = function() {
   var passwordLength = 0;
   while (!passwordLength || passwordLength < 8 || passwordLength > 124) {
@@ -15,6 +14,7 @@ var getPasswordLength = function() {
   return passwordLength;
 };
 
+//Confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
 var getValidCharacters = function() {
   var validCharacters = [];
   var validCharacterSelection = false;
@@ -41,43 +41,34 @@ var getValidCharacters = function() {
 };
 
 var generatePassword = function() {
-  //defaults
+  //Default Criteria
   var passwordLength = 8;
   var validCharacters = lowerCaseArray;
 
-  //present a series of prompts for password criteria
-  var passwordCriteria = window.prompt("Which criteria would you like to include in the password: Please enter one 1 for PASSWORD LENGTH, 2 for CHARACTER TYPE, or 3 for BOTH.");  
-
-  //Select which criteria to include in the password
+  //Prompt for Password Criteria
+  var passwordCriteria = window.prompt("Which criteria would you like to include in the generated password? Please enter one 1 for PASSWORD LENGTH, 2 for CHARACTER TYPE, or 3 for BOTH.");  
   switch (parseInt(passwordCriteria)) {
       case 1:
-          //Prompted for the length of the password, at least 8 characters and no more than 128 characters
           passwordLength = getPasswordLength();
-          console.log(passwordLength);
           break;
       case 2:
-          //WHEN asked for character types to include in the password THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
           validCharacters = getValidCharacters();
-          console.log(validCharacters);
           break;
       case 3:
-          //Prompted for the length of the password, at least 8 characters and no more than 128 characters
           passwordLength = getPasswordLength();
           validCharacters = getValidCharacters();
-          console.log(passwordLength);
-          console.log(validCharacters);
           break;
       default:
-          return generatePassword();
+        //if answer is not valid, prompt again  
+        return generatePassword();
   }
 
-  var password = "";
+  var generatedPassword = "";
   for (i = 0; i < passwordLength; i++) {
     var randomCharacter = validCharacters[Math.floor(Math.random() * (validCharacters.length))];
-    console.log(randomCharacter);
-    password = password + randomCharacter;
+    generatedPassword = generatedPassword + randomCharacter;
   }
-  return password;
+  return generatedPassword;
 }
 
 
@@ -86,12 +77,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  console.log("you clicked the gen button!");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
